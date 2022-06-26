6732bbdda413f7badd3ee1d1fe2c12ed4569d95a
Aryan Sethi <46134586+neohacker18@users.noreply.github.com>
2022-06-26 16:19:10 +0530

    init

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBBI-oXWPZn99uCek-FXrKYjbsQgnesoOY",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth=getAuth(app)
+
+export {auth};
